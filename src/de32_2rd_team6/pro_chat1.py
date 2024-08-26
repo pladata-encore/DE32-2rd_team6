43e0a121d4363bd5ccb000de47f3ab3c1fe12ee4
thephunkmonk <andrewchoi1218@gmail.com>
2024-08-26 12:18:39 +0900

    added USERs prompt

--- src/de32_2rd_team6/pro_chat1.py
+++ src/de32_2rd_team6/pro_chat1.py
@@ -12,7 +12,7 @@ print("채팅 프로그램- 메시지 발신자")
 print("메시지를 입력하세요. (종료시 'exit' 입력)")
 
 while True:
-    msg = input("YOU: ")
+    msg = input("USER1: ")
     if msg.lower() == 'exit':
         break
     data = {'message' : msg, 'time' : time.time()}
