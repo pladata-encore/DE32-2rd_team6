54ae06ccf55e5f573d166170ca9dcd78e0d59474
EstherCho-7 <dkgkqm777@gmail.com>
2024-08-28 12:37:02 +0900

    add chatbot

--- src/de32_2rd_team6/test_chatbot.py
+++ src/de32_2rd_team6/test_chatbot.py
@@ -1,4 +1,5 @@
 import os
+import time
 import json
 from kafka import KafkaConsumer, KafkaProducer
 
@@ -18,18 +19,19 @@ def search_movie_in_directories(base_dir, movie_name):
 # Kafka producer를 설정하는 함수
 def send_response_to_kafka(response_message):
     producer = KafkaProducer(
-        bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
+        #bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
+        bootstrap_servers=['localhost:9092'],
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
-    producer.send('haha', value=response_message)
+    producer.send('test', value=response_message)
     producer.flush()
     producer.close()
 
 # Kafka 메시지를 소비하고, 영화 데이터를 검색한 후, 응답을 전송하는 함수
 def consume_messages():
     consumer = KafkaConsumer(
-        'haha',
-        bootstrap_servers=['ec2-43-203-210-250.ap-northeast-2.compute.amazonaws.com:9092'],
+        'test',
+        bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest',
         #enable_auto_commit=True,
         #group_id='chatbot-group',
@@ -40,15 +42,17 @@ def consume_messages():
 
     for message in consumer:
         data = message.value
+        username='챗봇'
         if 'message' in data:
             user_input = data['message']
             if user_input.startswith('@챗봇'):
                 movie_name = user_input.split('@챗봇', 1)[-1].strip()
                 movie_data = search_movie_in_directories(base_dir, movie_name)
+                json_movie_data = json.dumps(movie_data, indent=4, ensure_ascii=False)
                 if movie_data:
-                    response = {"status": "success", "data": movie_data}
+                    response = {"username": username, "message": json_movie_data, 'time': time.time()}
                 else:
-                    response = {"status": "error", "message": f"영화 '{movie_name}'를 찾을 수 없습니다."}
+                    response = {"username": username, "message": f"영화 '{movie_name}'를 찾을 수 없습니다.", 'time': time.time()}
                 
                 # 결과를 출력하고, Kafka로 전송
                 send_response_to_kafka(response)
